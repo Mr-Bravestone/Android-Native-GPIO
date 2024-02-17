bb41d8c923ac2e5f34c7df9b70312b8fd08ef411
Mr-Bravestone <reversesplash68@gmail.com>
2024-02-17 22:45:38 +0530

    Update

--- Android-GPIO/src/main/java/mr/bravestone/android_gpio/RootCheck.kt
+++ Android-GPIO/src/main/java/mr/bravestone/android_gpio/RootCheck.kt
@@ -3,9 +3,18 @@ package mr.bravestone.android_gpio
 class RootCheck {
     fun Ability(): Int {
         try{
-            ShellExec("su -c ls")
-            println("Rooted")
-            return 1
+            var result = ShellExec("su -c ls").Status
+            if(result == "pass")
+            {
+                println("Rooted")
+                return 1
+            }
+            else
+            {
+                println("RootNotFound")
+                return 0
+            }
+
         }catch(e: Exception){
             println("RootNotFound")
             return 0
--- app/src/main/java/com/example/android_native_gpio/MainActivity.kt
+++ app/src/main/java/com/example/android_native_gpio/MainActivity.kt
@@ -1,14 +1,43 @@
 package com.example.android_native_gpio
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import mr.bravestone.android_gpio.GPIO
 import mr.bravestone.android_gpio.PCF8574
+import mr.bravestone.android_gpio.RootCheck
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val chkRootBtn = findViewById<Button>(R.id.chkRoot)
+        val pcfBtn = findViewById<Button>(R.id.pcfBtn)
+        val obgpioBtn = findViewById<Button>(R.id.obgpio)
+        chkRootBtn.setOnClickListener {
+            println(RootCheck().Ability())
+            if (RootCheck().Ability() == 1)
+            {
+                chkRootBtn.visibility = View.INVISIBLE
+                pcfBtn.visibility = View.VISIBLE
+                obgpioBtn.visibility = View.VISIBLE
+                Toast.makeText(this,"Device is Rooted",Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                Toast.makeText(this,"Device Not Rooted",Toast.LENGTH_SHORT).show()
+            }
+        }
+        pcfBtn.setOnClickListener {
+
+
+        }
+
 
     }
 }
